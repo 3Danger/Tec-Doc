40c8d9a2cf6503f1398a8e3cc1c8b0c690338aba
3danger <3danger@mail.ru>
2022-06-16 17:20:51 +0300

    remove unused field LogTimeFormat from Config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,5 @@ package config
 import _ "github.com/kelseyhightower/envconfig"
 
 type Config struct {
-	LogLevel      string `envconfig:"LOG_LEVEL"`
-	LogTimeFormat string `envconfig:"LOG_TM_FORMAT"`
+	LogLevel string `envconfig:"LOG_LEVEL"`
 }
