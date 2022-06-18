72ce5db7cc93b4bdcdc3a03daedbb771250f2a4b
tolkachev <alexzanser@mail.ru>
2022-06-18 23:24:42 +0300

    getBrands getArticles

--- pkg/clients/tecdoc/client.go
+++ pkg/clients/tecdoc/client.go
@@ -1,18 +1,21 @@
 package tecdoc
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"tec-doc/internal/config"
 	"tec-doc/internal/model"
 	"time"
 )
 
 //Client интерфейс с методами для получения запчастей с TecDoc
 type TecDocClient interface {
-	GetAllParts(ctx *context.Context, ID []string) ([]model.Autopart, error)
+	GetArticles(ctx *context.Context, tecDocCfg config.TecDocConfig, mfrName string) ([]model.Article, error)
+	GetBrand(ctx *context.Context, tecDocCfg config.TecDocConfig, mfrName string) (model.Brand, error)
 }
 
 type tecDocClient struct {
@@ -27,14 +30,18 @@ func NewClient(baseURL string, timeout time.Duration) (*tecDocClient, error) {
 	}, nil
 }
 
-func (c *tecDocClient) GetAllParts(ctx *context.Context, ID []string) ([]model.Autopart, error) {
-	req, err := http.NewRequest(http.MethodGet, c.baseURL, nil)
+func (c *tecDocClient) GetBrand(ctx *context.Context, tecDocCfg config.TecDocConfig, mfrName string) (*model.Brand, error) {
+	reqBodyReader := bytes.NewReader([]byte(fmt.Sprintf(
+		`{"getBrands":{"articleCountry":"ru", "lang":"ru", "provider":%s}}`, tecDocCfg.ProviderId)))
+
+	req, err := http.NewRequest(http.MethodPost, tecDocCfg.URL, reqBodyReader)
 	if err != nil {
 		return nil, fmt.Errorf("can't create new request: %v", err)
 	}
-	req.Header.Set("Content-Type", "application/json")
 
-	//Здесь будет что-то, добавляющее в запрос ID запчастей
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Api-Key", tecDocCfg.XApiKey)
+	//Language code????
 
 	resp, err := c.Do(req)
 	if err != nil {
@@ -46,14 +53,78 @@ func (c *tecDocClient) GetAllParts(ctx *context.Context, ID []string) ([]model.A
 		return nil, fmt.Errorf("can't read response")
 	}
 
-	defer resp.Body.Close()
+	type respStruct struct {
+		Data struct {
+			Array []model.Brand `json:"array"`
+		} `json:"data"`
+		Status int `json:"status"`
+	}
 
-	var parts []model.Autopart
+	var r respStruct
 
-	err = json.Unmarshal(body, &parts)
+	err = json.Unmarshal(body, &r)
 	if err != nil {
 		return nil, fmt.Errorf("can't unmarshal body: %v", err)
 	}
 
-	return parts, nil
+	for _, brand := range r.Data.Array {
+		if brand.MfrName == mfrName {
+			return &brand, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no brand found")
+
+}
+
+func (c *tecDocClient) GetArticles(ctx *context.Context, tecDocCfg config.TecDocConfig, dataSupplierId int, article string) ([]model.Article, error) {
+	reqBodyReader := bytes.NewReader([]byte(fmt.Sprintf(
+		`{
+			"getArticles": {
+				"articleCountry":"ru", 
+				"searchQuery": "%s",
+				"searchType": 10,
+				"dataSupplierIds": %d,
+				"lang":"ru"
+			}
+		}`, article, dataSupplierId)))
+
+	req, err := http.NewRequest(http.MethodPost, tecDocCfg.URL, reqBodyReader)
+	if err != nil {
+		return nil, fmt.Errorf("can't create new request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Api-Key", tecDocCfg.XApiKey)
+	//Language code????
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("can't get response: %v", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("can't read response")
+	}
+
+	type respStruct struct {
+		TotalMatchingArticles int             `json:"totalMatchingArticles"`
+		MaxAllowedPage        int             `json:"maxAllowedPage"`
+		Articles              []model.Article `json:"articles"`
+		Status                int             `json:"status"`
+	}
+
+	var r respStruct
+
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		return nil, fmt.Errorf("can't unmarshal body: %v", err)
+	}
+
+	if len(r.Articles) == 0 {
+		return nil, fmt.Errorf("no articles found")
+	}
+
+	return r.Articles, nil
 }
