b9d32724ee0bc9277f8a6f7e94745db43a87b3d8
alexzanser <alexzanser@mail.ru>
2022-07-25 22:01:16 +0300

    minor fixes and enhancements after review

---
+++ internal/tec-doc/web/externalserver/excel.go
@@ -0,0 +1,51 @@
+package externalserver
+
+import (
+	"errors"
+	exl "github.com/xuri/excelize/v2"
+	"io"
+	"strconv"
+	"tec-doc/internal/tec-doc/model"
+)
+
+func (e *externalHttpServer) loadFromExcel(bodyData io.Reader) (products []model.Product, err error) {
+	var rows [][]string
+	f, err := exl.OpenReader(bodyData)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = f.Close() }()
+	list := f.GetSheetList()
+	if len(list) == 0 {
+		return nil, errors.New("empty data")
+	}
+	rows, err = f.GetRows("Products")
+	if len(rows) < 2 {
+		return nil, errors.New("empty data")
+	}
+	products = make([]model.Product, len(rows[1:]))
+	for i := range products {
+		if err = e.parseExcelRow(&products[i], rows[i+1]); err != nil {
+			return nil, err
+		}
+	}
+	return products, nil
+}
+
+func (e *externalHttpServer) parseExcelRow(p *model.Product, row []string) (err error) {
+	if len(row) < 7 {
+		return errors.New("row is invalid")
+	}
+	if p.CardNumber, err = strconv.Atoi(row[0]); err != nil {
+		return err
+	}
+	p.ProviderArticle = row[1]
+	p.ManufacturerArticle = row[2]
+	p.Brand = row[3]
+	p.SKU = row[4]
+	p.Category = row[5]
+	if p.Price, err = strconv.Atoi(row[6]); err != nil {
+		return err
+	}
+	return nil
+}
