c1cd0fb83473638291f817e22c631c73f2043bb2
alexzanser <alexzanser@mail.ru>
2022-08-29 10:45:16 +0300

    fixes merge

--- pkg/clients/model/tecdoc.go
+++ pkg/clients/model/tecdoc.go
@@ -1,6 +1,8 @@
 package model
 
-import "time"
+import (
+	"time"
+)
 
 type Brand struct {
 	SupplierId int    `json:"dataSupplierId"`
@@ -98,14 +100,6 @@ type OEM struct {
 	MfrName       string `json:"mfrName"`
 }
 
-type LinkageTargets struct {
-	LinkageTargetId        int    `json:"linkageTargetId"`
-	MfrName                string `json:"mfrName"`
-	VehicleModelSeriesName string `json:"vehicleModelSeriesName"`
-	BeginYearMonth         string `json:"beginYearMonth"`
-	EndYearMonth           string `json:"endYearMonth"`
-}
-
 type AssemblyGroupFacets struct {
 	Total  int `json:"total"`
 	Counts []struct {
@@ -146,4 +140,49 @@ type Product struct {
 	UpdateDate          time.Time `json:"updateDate"`
 	Status              int       `json:"status"`
 	ErrorResponse       string    `json:"errorResponse"`
+}
+
+type LinkageTargets struct {
+	LinkageTargetId        int    `json:"linkageTargetId"`
+	MfrName                string `json:"mfrName"`
+	VehicleModelSeriesName string `json:"vehicleModelSeriesName"`
+	BeginYearMonth         string `json:"beginYearMonth"`
+	EndYearMonth           string `json:"endYearMonth"`
+}
+
+type (
+	// GetLinkageTargetsResponse для запроса
+	GetLinkageTargets struct {
+		PerPage              int              `json:"perPage"`
+		Page                 int              `json:"page"`
+		LinkageTargetCountry string           `json:"linkageTargetCountry"`
+		Lang                 string           `json:"lang"`
+		LinkageTargetIds     []map[string]any `json:"linkageTargetIds"`
+	}
+	GetLinkageTargetsResponse struct {
+		GetLinkageTargets GetLinkageTargets `json:"getLinkageTargets"`
+	}
+)
+
+// Data для записи ответа первого запроса
+type Data struct {
+	Data struct {
+		Array []ArticleLinkages `json:"array"`
+	} `json:"data"`
+	Status int `json:"status"`
+}
+
+type ArticleLinkages struct {
+	ArticleLinkages struct {
+		LinkingTargetId []struct {
+			LinkingTargetId int `json:"linkingTargetId"`
+		} `json:"array"`
+	} `json:"articleLinkages"`
+}
+
+// LinkageTargetsResponse для записи результата
+type LinkageTargetsResponse struct {
+	Total          int              `json:"total"`
+	LinkageTargets []LinkageTargets `json:"linkageTargets"`
+	Status         int              `json:"status"`
 }
--- pkg/clients/tecdoc/applicability.go
+++ pkg/clients/tecdoc/applicability.go
@@ -6,21 +6,21 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
-	"tec-doc/pkg/clients/models"
+	"tec-doc/pkg/clients/model"
 )
 
 // LIMIT Лимит на единицу запроса
 const LIMIT = 100
 
 // Applicability получение списка применимости
-func (c *tecDocClient) Applicability(legacyArticleId int) ([]models.LinkageTargets, error) {
+func (c *tecDocClient) Applicability(legacyArticleId int) ([]model.LinkageTargets, error) {
 	linkageTargetsBodies, err := c.getLinkageTargetsResponse(legacyArticleId)
 	if err != nil {
 		return nil, err
 	}
 
 	length := len(linkageTargetsBodies)
-	targetCh := make(chan []models.LinkageTargets, length)
+	targetCh := make(chan []model.LinkageTargets, length)
 	errChan := make(chan error, length+1)
 	wg := new(sync.WaitGroup)
 
@@ -30,7 +30,7 @@ func (c *tecDocClient) Applicability(legacyArticleId int) ([]models.LinkageTarge
 		defer close(errChan)
 		for i := range linkageTargetsBodies {
 			wg.Add(1)
-			go func(wg *sync.WaitGroup, LinkageTargets models.GetLinkageTargetsResponse) {
+			go func(wg *sync.WaitGroup, LinkageTargets model.GetLinkageTargetsResponse) {
 				defer wg.Done()
 				targets, err := c.getLinkageTargets(LinkageTargets)
 				if err != nil {
@@ -44,7 +44,7 @@ func (c *tecDocClient) Applicability(legacyArticleId int) ([]models.LinkageTarge
 		wg.Wait()
 		errChan <- nil
 	}(wg)
-	linkageTargets := make([]models.LinkageTargets, 0, length*LIMIT)
+	linkageTargets := make([]model.LinkageTargets, 0, length*LIMIT)
 	for target := range targetCh {
 		linkageTargets = append(linkageTargets, target...)
 	}
@@ -54,10 +54,10 @@ func (c *tecDocClient) Applicability(legacyArticleId int) ([]models.LinkageTarge
 	return linkageTargets, nil
 }
 
-func (c *tecDocClient) getLinkageTargetsResponse(legacyArticleId int) ([]models.GetLinkageTargetsResponse, error) {
+func (c *tecDocClient) getLinkageTargetsResponse(legacyArticleId int) ([]model.GetLinkageTargetsResponse, error) {
 	var (
-		LinkageTargetsBody []models.GetLinkageTargetsResponse
-		linkages           []models.ArticleLinkages
+		LinkageTargetsBody []model.GetLinkageTargetsResponse
+		linkages           []model.ArticleLinkages
 		err                error
 	)
 	if linkages, err = c.getArticleLinkedAllLinkingTarget4(legacyArticleId); err != nil {
@@ -75,15 +75,15 @@ func (c *tecDocClient) getLinkageTargetsResponse(legacyArticleId int) ([]models.
 	steps := len(linkageTargetIds) / LIMIT
 	for i := 0; i < steps; i++ {
 		start, end := i*LIMIT, (i+1)*LIMIT
-		LinkageTargetsBody = append(LinkageTargetsBody, models.GetLinkageTargetsResponse{
-			GetLinkageTargets: models.GetLinkageTargets{
+		LinkageTargetsBody = append(LinkageTargetsBody, model.GetLinkageTargetsResponse{
+			GetLinkageTargets: model.GetLinkageTargets{
 				PerPage: LIMIT, Page: 1,
 				LinkageTargetCountry: "RU", Lang: "ru",
 				LinkageTargetIds: linkageTargetIds[start:end]},
 		})
 	}
-	LinkageTargetsBody = append(LinkageTargetsBody, models.GetLinkageTargetsResponse{
-		GetLinkageTargets: models.GetLinkageTargets{
+	LinkageTargetsBody = append(LinkageTargetsBody, model.GetLinkageTargetsResponse{
+		GetLinkageTargets: model.GetLinkageTargets{
 			PerPage: LIMIT, Page: 1,
 			LinkageTargetCountry: "RU", Lang: "ru",
 			LinkageTargetIds: linkageTargetIds[steps*LIMIT:]},
@@ -91,8 +91,8 @@ func (c *tecDocClient) getLinkageTargetsResponse(legacyArticleId int) ([]models.
 	return LinkageTargetsBody, nil
 }
 
-func (c *tecDocClient) getArticleLinkedAllLinkingTarget4(legacyArticleId int) ([]models.ArticleLinkages, error) {
-	var responseFirst models.Data
+func (c *tecDocClient) getArticleLinkedAllLinkingTarget4(legacyArticleId int) ([]model.ArticleLinkages, error) {
+	var responseFirst model.Data
 	{
 		requestBody := bytes.NewReader([]byte(fmt.Sprintf(
 			`{
@@ -117,11 +117,11 @@ func (c *tecDocClient) getArticleLinkedAllLinkingTarget4(legacyArticleId int) ([
 
 // getLinkageTargets
 // максимальный размер массива внутри структуры LinkageTargetIds = 100 (указал в константе LIMIT)
-func (c *tecDocClient) getLinkageTargets(LinkageTargetsBody models.GetLinkageTargetsResponse) (linkageTargets []models.LinkageTargets, err error) {
+func (c *tecDocClient) getLinkageTargets(LinkageTargetsBody model.GetLinkageTargetsResponse) (linkageTargets []model.LinkageTargets, err error) {
 	var length = len(LinkageTargetsBody.GetLinkageTargets.LinkageTargetIds)
-	linkageTargets = make([]models.LinkageTargets, 0, length)
+	linkageTargets = make([]model.LinkageTargets, 0, length)
 	LinkageTargetsBody.GetLinkageTargets.Page = 1
-	var linkageTargetsResponse = models.LinkageTargetsResponse{Total: length}
+	var linkageTargetsResponse = model.LinkageTargetsResponse{Total: length}
 	for arrivedCount := 0; arrivedCount < linkageTargetsResponse.Total; {
 		var requestByte []byte
 		if requestByte, err = json.Marshal(LinkageTargetsBody); err != nil {
