26d4c67b9c4d612c94fbd6b26d6ab9986dfa63ba
magomedov.m25 <magomedrasul7@gmail.com>
2022-09-02 14:27:11 +0300

    Use buffer for GetProductsEnrichedExcel()

--- internal/tec-doc/service/excel.go
+++ internal/tec-doc/service/excel.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	exl "github.com/xuri/excelize/v2"
 	"tec-doc/pkg/model"
@@ -88,84 +90,69 @@ func (s *Service) AddFromExcel(ctx *gin.Context, products []model.Product, suppl
 }
 
 func (s *Service) GetProductsEnrichedExcel(productsPoor []model.Product) (data []byte, err error) {
-	products, err := s.tecDocClient.Enrichment(productsPoor)
-	if err != nil {
+	var productsEnriched []model.ProductEnriched
+
+	if productsEnriched, err = s.tecDocClient.Enrichment(productsPoor); err != nil {
 		return nil, err
 	}
+
 	f := exl.NewFile()
 	defer func() { _ = f.Close() }()
-	nameSheet := "Details about products"
-	f.SetSheetName(f.GetSheetName(0), nameSheet)
+	f.SetSheetName(f.GetSheetName(0), "Details about products")
+	sw, err := f.NewStreamWriter(f.GetSheetName(0))
 
-	// Set styles & length
-	styleHeaderId, err := f.NewStyle(styleExcelHeader)
-	if err != nil {
-		return nil, err
-	}
-	styleId, err := f.NewStyle(styleExcel)
-	if err != nil {
-		return nil, err
-	}
-	{
-		// Set header into excel file
-		//TODO узнать в чем дело, почему первый стиль перекрывает собой другой стиль
-		_ = f.SetRowStyle(nameSheet, 1, 2, styleHeaderId)
-		_ = f.SetRowStyle(nameSheet, 2, 10, styleId)
-		setWidths := func(width []float64) (err error) {
-			for i := 0; i < len(width) && err == nil; i++ {
-				a, b := rune('A'+i), rune('A'+i+1)
-				err = f.SetColWidth(nameSheet, string(a), string(b), width[i])
-			}
-			return
-		}
-		if err = setWidths([]float64{14, 15, 25, 25, 18, 18, 40, 40, 40, 40, 40, 40}); err != nil {
+	// Set width for columns
+	for i, w := range []float64{14, 15, 25, 25, 18, 13, 25, 50, 40, 40, 8, 40, 40} {
+		i++
+		if err = sw.SetColWidth(i, i, w); err != nil {
 			return nil, err
 		}
-		setValue := func(x, y int, value interface{}) {
-			axis, _ := exl.CoordinatesToCellName(x, y)
-			_ = f.SetCellValue(nameSheet, axis, value)
-		}
-
-		// Add values from struct Products
-		setValue(1, 1, "Бренд")
-		setValue(2, 1, "Категория")
-		setValue(3, 1, "Артикул поставщика (уникальный артикул)")
-		setValue(4, 1, "Артикул производителя (артикул tec-doc)")
-		setValue(5, 1, "Штрих-код")
-		setValue(6, 1, "Цена товара")
-
-		// Add values from struct Articles
-		setValue(7, 1, "Описание")
-		setValue(8, 1, "Cross numbers")
-		setValue(9, 1, "Размерность")
-		setValue(10, 1, "ArticleCriterias")
-		setValue(11, 1, "PackageArticleCriterias")
-		setValue(12, 1, "Применимости")
-		setValue(13, 1, "Изображения")
-
-		for i, p := range products {
-			i += 2
-			setValue(1, i, p.Product.Brand)
-			setValue(2, i, p.Product.Subject)
-			setValue(3, i, p.Product.ArticleSupplier)
-			setValue(4, i, p.Product.Article)
-			setValue(5, i, p.Product.Barcode)
-			setValue(6, i, p.Product.Price)
-
-			setValue(7, i, p.Article.GenericArticleDescription)
-			setValue(8, i, p.Article.OEMnumbers)
-			setValue(9, i, p.Article.CrossNumbers)
-			setValue(10, i, p.Article.ArticleCriteria)
-			setValue(11, i, p.Article.PackageArticleCriteria)
-			setValue(12, i, p.Article.LinkageTargets)
-			setValue(13, i, p.Article.Images)
-		}
-		err = f.Save()
-
 	}
-	//A B C D E F G H I K L M N O P Q R S T V X Y Z
-	buffer, err := f.WriteToBuffer()
-	if err != nil {
+	if err = sw.SetRow("A1", []interface{}{
+		"Бренд",
+		"Категория",
+		"Артикул поставщика (уникальный артикул)",
+		"Артикул производителя (артикул tec-doc)",
+		"Штрих-код",
+		"Цена товара",
+		"Описание",
+		"Cross numbers",
+		"Размерность",
+		"ArticleCriterias",
+		"PackageArticleCriterias",
+		"Применимости",
+		"Изображения",
+	}, exl.RowOpts{Height: 15},
+	); err != nil {
+		return nil, err
+	}
+
+	for i, p := range productsEnriched {
+		axis := fmt.Sprintf("A%d", i+2)
+		err = sw.SetRow(axis, []interface{}{
+			p.Product.Brand,
+			p.Product.Subject,
+			p.Product.ArticleSupplier,
+			p.Product.Article,
+			p.Product.Barcode,
+			p.Product.Price,
+			p.Article.GenericArticleDescription,
+			p.Article.OEMnumbers,
+			p.Article.CrossNumbers,
+			p.Article.ArticleCriteria,
+			p.Article.PackageArticleCriteria,
+			p.Article.LinkageTargets,
+			p.Article.Images},
+			exl.RowOpts{Height: 15})
+		if err != nil {
+			return nil, err
+		}
+	}
+	if err = sw.Flush(); err != nil {
+		return nil, err
+	}
+	var buffer *bytes.Buffer
+	if buffer, err = f.WriteToBuffer(); err != nil {
 		return nil, err
 	}
 	return buffer.Bytes(), nil
